ad70b1f1fd0c86e3abbbe1de73d1bf3ede82c01f
leonardoserra <serra.leonardo92@gmail.com>
2023-03-06 12:14:16 +0100

    creo funzione per n casuale di bombe

--- js/main.js
+++ js/main.js
@@ -1,12 +1,25 @@
-/* Consegna
-     L’utente clicca su un bottone che genererà
-        una griglia di gioco quadrata.
-     Ogni cella ha un numero progressivo, da 1 a 100.
-     Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-     Quando l’utente clicca su ogni cella, 
-        la cella cliccata si colora di azzurro ed emetto 
-        un messaggio in console con il numero della cella 
-        cliccata.
+    /* Consegna
+        Il computer deve generare 16 numeri casuali nello stesso 
+            range della difficoltà prescelta: le bombe. Attenzione: 
+            nella stessa cella può essere posizionata al massimo una 
+            bomba, perciò nell’array delle bombe non potranno esserci
+            due numeri uguali.
+        In seguito l’utente clicca su una cella: se il numero
+            è presente nella lista dei numeri generati - abbiamo
+            calpestato una bomba - la cella si colora di rosso e
+            la partita termina. Altrimenti la cella cliccata si
+            colora di azzurro e l’utente può continuare a
+            cliccare sulle altre celle.
+        La partita termina quando il giocatore 
+            clicca su una bomba o quando raggiunge il 
+            numero massimo possibile di numeri consentiti
+            (ovvero quando ha rivelato tutte le celle che 
+            non sono bombe).
+        Al termine della partita il software deve 
+            comunicare il punteggio, cioè il numero di
+            volte che l’utente ha cliccato su una cella
+            che non era una bomba.
+
 
      Bonus
      Aggiungere una select accanto al bottone di
@@ -24,7 +37,7 @@
 const playField = document.getElementById('playField');
 const startButton = document.getElementById('button');
 const difficulty = document.getElementById('difficulty');
-
+const bombs = [];
 
 startButton.addEventListener('click', function(){
 
@@ -71,6 +84,15 @@ function squareGenerator(numberInside){
 
 }
 
+
+function bombsNumbersGen(arrayToFill, randomNumberRange){
+    for(c = 0; c < 16; c++){
+    arrayToFill.push(Math.floor(Math.random) * randomNumberRange + 1);
+    }
+
+}
+
+console.log(bombs);
 
 
 
